80bddacbb77efbeab6eeed939b416756257e5fe6
Luiz Henrique <lui123henri@hotmail.com>
2024-03-12 21:17:53 -0300

    Create Detector de sardinha.py

---
+++ 002.5 - Sardine Detector/Detector de sardinha.py
@@ -0,0 +1,7 @@
+# Detector de sardinha
+
+respostasardinha = input("Você é uma sardinha?")
+if respostasardinha.lower()!= "s" and respostasardinha.lower()!= "sim":
+    print("Você NÃO é uma sardinha")
+else:
+    print("Você é uma sardinha") 
